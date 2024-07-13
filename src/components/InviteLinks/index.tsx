4f74e96c4698bd4ebfd542903b8425c7f46e0a2d
Rahul P S <rahulps1000@gmail.com>
2024-07-13 16:11:55 +0530

    bug fixes

--- src/components/InviteLinks/index.tsx
+++ src/components/InviteLinks/index.tsx
@@ -1,42 +1,50 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
+import { useParams } from "next/navigation";
 import styles from "./styles.module.css";
 import CopyBox from "../CopyBox";
 import Loader from "../Loader";
 
-const InviteLinks = () => {
+const InviteLinks = ({ userId }: { userId: string }) => {
+  const { repo_id } = useParams<{ repo_id: string }>();
+
   const invites = [];
   const [showPopup, setPopup] = useState<boolean>(false);
   const [inviteLink, setInviteLink] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+
+  const generateInviteLink = async () => {
+    setPopup(true);
+  };
   const closePopup = () => {
     setInviteLink(null);
     setPopup(false);
   };
   return (
-    <div className={styles.card}>
-      <div className={styles.cardHeader}>
-        <h3>Invite Links</h3>
-        <div className={styles.content}>
-          <div className={styles.button}>
-            <span className="material-symbols-outlined">add</span>
-            Generate Invite Link
+    <>
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <h3>Invite Links</h3>
+          <div className={styles.content}>
+            <div className={styles.button} onClick={generateInviteLink}>
+              <span className="material-symbols-outlined">add</span>
+              Generate Invite Link
+            </div>
           </div>
         </div>
+        <hr />
+        <div className="invites">
+          {invites.length > 0 ? (
+            "invites"
+          ) : (
+            <div className={styles.noInvite}>No Invite Links Generated</div>
+          )}
+        </div>
       </div>
-      <hr />
-      <div className="invites">
-        {invites.length > 0 ? (
-          "invites"
-        ) : (
-          <div className={styles.noInvite}>No Invite Links Generated</div>
-        )}
-      </div>
-      </div>
-      {showPopup && inviteLink && (
-      <div className={styles.popup}>
-        <div className={styles.card}>
+      {showPopup && (
+        <div className={styles.popup}>
+          <div className={styles.card}>
             <span className="material-symbols-outlined" onClick={closePopup}>
               close
             </span>
